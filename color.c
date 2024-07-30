3487869a630fe834d25892fc28d4962bb7071339
Rakshith08URK <rajrakshith08@gmail.com>
2024-07-30 15:14:59 +0530

    Update color.c

--- color.c
+++ color.c
@@ -1,11 +1,12 @@
 #include "color.h"
+#include <stdio.h>
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
+int GetColorFromPairNumber(int pairNumber) {
+    int colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
     colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
